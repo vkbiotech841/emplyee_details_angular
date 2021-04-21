ac5b3d8d62fde2b3869db3e79deae1d92f371b22
VIKRAM KUMAR <vkbiotech841@gmail.com>
2021-04-21 08:44:20 +0530

    initial project  framework initiated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
+import { EmployeeDetailComponent } from './pages/employee-detail/employee-detail.component';
+import { EmployeeFormComponent } from './pages/employee-form/employee-form.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", component: EmployeeFormComponent },
+  { path: "employee", component: EmployeeDetailComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EmployeeFormComponent } from './pages/employee-form/employee-form.component';
+import { EmployeeDetailComponent } from './pages/employee-detail/employee-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmployeeFormComponent,
+    EmployeeDetailComponent
   ],
   imports: [
     BrowserModule,
